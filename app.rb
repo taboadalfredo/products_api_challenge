cde8a80fc410b5a02e4f95097dbc1ecac3f46251
Alfredo Taboada <taboadalfredo@gmail.com>
2025-04-01 05:04:29 -0300

    add static docs with it's cache time

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ require 'sinatra'
 require_relative 'config/initializers'
 require_relative 'routes/auth_routes'
 require_relative 'routes/product_routes'
+
+get '/openapi.yml' do
+  response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, proxy-revalidate'
+  response.headers['Pragma'] = 'no-cache'
+  response.headers['Expires'] = '0'
+  send_file 'openapi.yml'
+end
+
+get '/AUTHORS' do
+  response.headers['Cache-Control'] = 'public, max-age=86400'  # 86400 segundos = 24 horas
+  send_file 'AUTHORS'
+end
